c55ff210b8130055f20e7883d1ea799106b7b019
ZacharyLandes <zach.g.landes@gmail.com>
2017-11-28 19:53:22 -0700

    finish it1 functionaility

--- app/controllers/bike_share_app.rb
+++ app/controllers/bike_share_app.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class BikeShareApp < Sinatra::Base
 
   get '/stations' do
@@ -9,14 +10,31 @@ class BikeShareApp < Sinatra::Base
     erb :new
   end
 
-  get '/stations/:id' do
+  get '/stations/:id/edit' do
     @station = Station.find(params[:id])
-    erb :show
+    erb :edit
   end
 
   post '/stations/new' do
     station = Station.create(params[:station])
     redirect '/stations'
   end
+
+  put '/stations/:id' do |id|
+    Station.update(id.to_i, params[:station])
+    redirect "/stations/#{id}"
+  end
+
+  get '/stations/:id' do
+    @station = Station.find(params[:id])
+    erb :show
+  end
+
+  delete '/stations/:id' do |id|
+    station = Station.destroy(id.to_i)
+    redirect '/stations'
+  end
+
+
 
 end
