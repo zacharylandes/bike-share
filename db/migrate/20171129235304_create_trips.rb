f8fe1c0f245e5384486521be17adb9f4ad0b49b3
ZacharyLandes <zach.g.landes@gmail.com>
2017-11-29 17:22:31 -0700

    adding trips

---
+++ db/migrate/20171129235304_create_trips.rb
@@ -0,0 +1,16 @@
+class CreateTrips < ActiveRecord::Migration[5.1]
+  def change
+    create_table :trips do |t|
+      t.integer    :duration
+      t.date    :start_date
+      t.text    :start_station
+      t.date    :end_date
+      t.text    :end_station
+      t.integer :bike_id
+      t.text    :subscription_type
+      t.integer :zipcode
+
+      # t.timestamps null: false
+    end
+  end
+end
