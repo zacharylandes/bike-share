c6453b6e22b473437633b4b9173e0c0549a6bf74
ZacharyLandes <zach.g.landes@gmail.com>
2017-12-02 15:56:48 -0700

    subscription breakout

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -15,7 +15,6 @@ class Trip < ActiveRecord::Base
 
 
   def self.average_trip_length
-    # binding.pry
     average(:duration).to_i
   end
 
@@ -35,16 +34,15 @@ class Trip < ActiveRecord::Base
   def self.station_with_most_rides_at_end
       max = group(:end_station_id).order("count_id DESC").count(:id).first[0]
       Station.find(max).name
-      # station.name
   end
 
   def self.years
     distinct.pluck('extract(year from start_date)')
   end
 
-def self.find_month_names(month)
-  Date::MONTHNAMES[month]
-end
+  def self.find_month_names(month)
+    Date::MONTHNAMES[month]
+  end
 
   def self.trips_per_month
     years.map do |year|
@@ -56,10 +54,32 @@ end
   end
 
   def self.sum_trips_per_year
-     result = years.map do |year|
-      found = where('extract(year from start_date)= ?', year).group('extract(month from start_date)').order("count_id DESC").count(:id)
-      end
-      result = result.map {|result| result.values.sum}
-      years.zip(result)
+    result = years.map do |year|
+     found = where('extract(year from start_date)= ?', year).group('extract(month from start_date)').order("count_id DESC").count(:id)
+    end
+    result = result.map {|result| result.values.sum}
+    years.zip(result)
+  end
+
+  def self.most_ridden_bike
+    group(:bike_id).order("count_id DESC").count(:id).first
+  end
+
+  def self.least_ridden_bike
+    group(:bike_id).order("count_id ASC").count(:id).first
+  end
+
+
+  def self.percentage_of_subscriber_type
+    amount = subscription_type_breakout.values
+    total = amount.sum
+    amount.map do |num|
+      ((num/total.to_f)*100).round(1)
+    end
+  end
+
+  def self.subscription_type_breakout
+     group(:subscription_type).order("count_id DESC").count(:id)
+    # binding.pry
   end
 end
