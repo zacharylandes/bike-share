c356a9edde815fc8c95ea40c729675c111fedfba
nico24687 <theonlynico@hotmail.com>
2017-12-03 20:18:50 -0700

    condition controller helper methods

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -1,5 +1,25 @@
 require 'pry'
 
 class Condition < ActiveRecord::Base
+  def average_number_of_rides(days)
+    
+  end
+  
+  private
+  def days_in_high_temperature_chunks
+    high_temperature_chunks.map do |temperature_range|
+      [temperature_range, days_in_temperature_range(temperature_range)]
+    end
+  end
 
+  def high_temperature_chunks
+    chunk_size = 10
+    min = (Condition.minimum(:max_temperature_f) / chunk_size).floor * chunk_size
+    max = (Condition.maximum(:max_temperature_f) / chunk_size).ceil * chunk_size
+    (min..max).step(chunk_size).map { |x| x..x+(chunk_size-1) }
+  end
+
+  def days_in_temperature_range(temperature_range)
+    Condition.distinct.where(max_temperature_f: temperature_range).pluck(:date)
+  end
 end
