4022ecd9aed32cc2b0de0f61073da494a4cf870d
ZacharyLandes <zach.g.landes@gmail.com>
2017-12-03 19:38:28 -0700

    add tests for conditions

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -2,4 +2,13 @@ require 'pry'
 
 class Condition < ActiveRecord::Base
 
+    validates_presence_of :max_temperature_f,
+                          :mean_temperature_f,
+                          :min_temperature_f,
+                          :mean_humidity,
+                          :mean_wind_speed_mph,
+                          :mean_visibility_miles,
+                          :mean_wind_speed_mph,
+                          :precipitation_inches
+
 end
---
+++ spec/features/user_visits_condition_index_spec.rb
@@ -0,0 +1,90 @@
+require 'spec_helper'
+
+describe "when a visitor visits the conditions index" do
+  it "sees a list of all the conditions" do
+    visit '/conditions'
+
+    expect(page).to have_content("Weather conditions")
+  end
+  #
+  it "can click the link to create a new condition" do
+    visit '/conditions'
+    click_link('Create a new condition')
+
+    expect(page).to have_current_path("/conditions/new")
+    expect(page).to have_content("Make a new condition")
+  end
+
+  it "clicks the name of the condition to display its information" do
+       Condition.create!(
+          date: '2013-08-31',
+          max_temperature_f: 2,
+          mean_temperature_f: 2,
+          min_temperature_f: 3,
+          mean_humidity: 3,
+          mean_visibility_miles: 4,
+          precipitation_inches: 2,
+          mean_wind_speed_mph:2
+        )
+    visit '/conditions'
+
+    click_link('2013-08-31')
+
+    expect(page).to have_content("Min Temp")
+  end
+
+  it "clicks edit condition of the condition to display the edit condition page" do
+       Condition.create!(
+          date: '2013-08-31',
+          max_temperature_f: 2,
+          mean_temperature_f: 2,
+          min_temperature_f: 3,
+          mean_humidity: 3,
+          mean_visibility_miles: 4,
+          precipitation_inches: 2,
+          mean_wind_speed_mph:2
+        )
+    visit '/conditions'
+
+    click_on('Edit')
+
+    expect(page).to have_content("Min Temp")
+  end
+
+  it "clicks delete condition of the condition to display the delete  condition index page" do
+       Condition.create!(
+          date: '2013-08-31',
+          max_temperature_f: 2,
+          mean_temperature_f: 2,
+          min_temperature_f: 3,
+          mean_humidity: 3,
+          mean_visibility_miles: 4,
+          precipitation_inches: 2,
+          mean_wind_speed_mph:2
+        )
+    visit '/conditions'
+
+    click_on('delete')
+
+    expect(page).to have_content("Weather conditions")
+  end
+
+  #
+  # it "clicks the edit link and is presented an edit form" do
+  #   Station.create(name: "The station", dock_count: 33, city: "San Francisco", installation_date: "04-03-1204")
+  #   visit '/conditions'
+  #   click_link('Edit')
+  #
+  #   expect(page).to have_content("Edit your station")
+  # end
+  #
+  # it "clicks the delete button which deletes station and redirects to index" do
+  #   Station.create(name: "The station", dock_count: 33, city: "San Francisco", installation_date: "04-03-1204")
+  #   visit '/conditions'
+  #   click_on('delete')
+  #
+  #   expect(page).to have_content("Bike Station Details")
+  #   expect(Station.count).to eq(0)
+  # end
+
+end
