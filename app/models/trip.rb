19a6f80c132a01e09c3707a14455e8c484783c24
Gabriel Afflitto <gafflitto92@gmail.com>
2017-12-05 15:05:08 -0700

    stations with most rides at end refactored

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -26,8 +26,17 @@ class Trip < ActiveRecord::Base
   end
 
   def self.station_with_most_rides_at_start
-      max = group(:start_station_id).order("count_id DESC").count(:id).first[0]
-      Station.find(max).name
+    select("trips.*, count(trips.id) AS count_all_trips")
+      .joins(:station)
+      .group(:start_station_id,:id)
+      .order("count_all_trips DESC").first
+  end
+
+  def self.station_with_most_rides_at_end
+    select("trips.*, count(trips.id) AS count_all_trips")
+      .joins(:station)
+      .group(:end_station_id, :id)
+      .order("count_all_trips DESC").first
   end
 
   def stations
@@ -42,31 +51,17 @@ class Trip < ActiveRecord::Base
     where(end_station_id: id).count
   end
 
-  def self.station_with_most_rides_at_end
-    max = group(:end_station_id).order("count_id DESC").count(:id).first[0]
-    Station.find(max).name
-  end
-
   def self.years
     distinct.pluck('extract(year from start_date)')
   end
 
-  def self.find_month_names(month)
-    Date::MONTHNAMES[month]
-  end
+  # def self.find_month_names(month)
+  #   Date::MONTHNAMES[month]
+  # end
 
   def self.trips_per_month_by_year(years)
     where('extract(year from start_date)= ?', years).group('extract(year from start_date)').group('extract(month from start_date)').order("count_id DESC").count(:id)
   end
-
-  # def self.trips_per_month
-  #   years.map do |year|
-  #     found = where('extract(year from start_date)= ?', year).group('extract(month from start_date)').order("count_id DESC").count(:id)
-  #     months = Hash[found.map{|month,count|[month.to_i,count]}]
-  #     month_names = Hash[months.map{|month,count|[find_month_names(month),count]}]
-  #     {year => month_names }
-  #   end
-  # end
 
   def self.sum_trips_per_year(years)
     where('extract(year from start_date)= ?', years).group('extract(year from start_date)').order("count_id DESC").count(:id)
@@ -79,7 +74,6 @@ class Trip < ActiveRecord::Base
   def self.least_ridden_bike
     group(:bike_id).order("count_id ASC").count(:id).first
   end
-
 
   def self.percentage_of_subscriber_type
     amount = subscription_type_breakout.values
