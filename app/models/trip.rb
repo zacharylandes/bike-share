57760dfc0a05e156a5af8e6097138a010c085684
Gabriel Afflitto <gafflitto92@gmail.com>
2017-12-05 17:59:00 -0700

    dates with most and least number of tests refactored

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -60,24 +60,21 @@ class Trip < ActiveRecord::Base
   # end
 
   def self.trips_per_month_by_year(years)
-    where('extract(year from start_date)= ?', years).group('extract(year from start_date)').group('extract(month from start_date)').order("count_id DESC").count(:id)
+    where('extract(year from start_date)= ?', years)
+      .group('extract(year from start_date)')
+      .group('extract(month from start_date)')
+      .order("count_id DESC").count(:id)
   end
 
   def self.sum_trips_per_year(years)
-    where('extract(year from start_date)= ?', years).group('extract(year from start_date)').order("count_id DESC").count(:id)
+    where('extract(year from start_date)= ?', years)
+    .group('extract(year from start_date)')
+    .order("count_id DESC").count(:id)
   end
 
   def self.most_and_least_ridden_bikes
     group(:bike_id).order("count_id DESC").count(:id).to_a
   end
-
-  # def self.percentage_of_subscriber_type
-  #   amount = subscription_type_breakout.values
-  #   total = amount.sum
-  #   amount.map do |num|
-  #     ((num/total.to_f)*100).round(1)
-  #   end
-  # end
 
   def self.subscription_type_breakout
     group(:subscription_type).order("count_id DESC").count(:id).to_a
@@ -96,23 +93,17 @@ class Trip < ActiveRecord::Base
   end
 
   def self.date_with_most_trips
-     counts = Hash.new(0)
-     date = group(:start_date).order("count_id DESC").count(:id)
-     all_dates = date.keys.map {|date|date.strftime("%m-%d-%y")}
-     all_dates.each do |date|
-       counts[date]+=1
-     end
-     counts.max_by {|k, v| v}
+    group('(EXTRACT(MONTH FROM start_date))::integer')
+      .group('(EXTRACT(DAY FROM start_date))::integer')
+      .group('(EXTRACT(YEAR FROM start_date))::integer')
+      .order('count_all').count.invert.max
   end
 
   def self.date_with_least_trips
-      counts = Hash.new(0)
-     date = group(:start_date).order("count_id DESC").count(:id)
-     all_dates = date.keys.map {|date|date.strftime("%m-%d-%y")}
-     all_dates.each do |date|
-       counts[date]+=1
-     end
-     counts.min_by {|k, v| v}
+    group('(EXTRACT(MONTH FROM start_date))::integer')
+      .group('(EXTRACT(DAY FROM start_date))::integer')
+      .group('(EXTRACT(YEAR FROM start_date))::integer')
+      .order('count_all').count.invert.min
   end
 
   def self.most_frequent_destination(station)
@@ -126,15 +117,24 @@ class Trip < ActiveRecord::Base
   end
 
   def self.date_with_highest_number_of_trips_started_at_this_station(station)
-    date = where(start_station_id: station).group('(EXTRACT(MONTH FROM start_date))::integer').group('(EXTRACT(DAY FROM start_date))::integer').group('(EXTRACT(YEAR FROM start_date))::integer').order('count_all').count.first.first
+    date = where(start_station_id: station)
+      .group('(EXTRACT(MONTH FROM start_date))::integer')
+      .group('(EXTRACT(DAY FROM start_date))::integer')
+      .group('(EXTRACT(YEAR FROM start_date))::integer')
+      .order('count_all').count.first.first
     date.join("/")
   end
 
   def self.most_frequent_zip_code_for_users_starting_trips_at_this_station(station)
-    where(start_station_id: station).group(:zip_code).order('count_id DESC').count(:id).first[0]
+    where(start_station_id: station)
+      .group(:zip_code)
+      .order('count_id DESC').count(:id).first[0]
   end
 
   def self.bike_id_most_frequently_starting_a_trip_at_this_station(station)
-    where(start_station_id: station).group(:bike_id).order('count_id DESC').count(:id).first[0]
+    where(start_station_id: station)
+      .group(:bike_id)
+      .order('count_id DESC')
+      .count(:id).first[0]
   end
 end
