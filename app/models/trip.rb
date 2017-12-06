5245e25060a267f8f4f7db0d2f16df64ec385d9a
nico24687 <theonlynico@hotmail.com>
2017-12-06 13:23:26 -0700

    allow for multi year analytics

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -3,13 +3,6 @@ class Trip < ActiveRecord::Base
 
   belongs_to :station, foreign_key: "start_station_id", class_name: "Station"
 
-  validates_presence_of :duration,
-                        :start_date,
-                        :start_station_name,
-                        :end_date,
-                        :end_station_name,
-                        :bike_id,
-                        :subscription_type
 
 
   def self.average_trip_length
